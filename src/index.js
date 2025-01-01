a8a395d12f870b197a5c45e5d03b80b17156a6d8
khalil <khalilm22393@gmail.com>
2025-01-01 15:18:12 +0200

    change host name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const DB_USER = 'root'
 const DB_PSW = 'example'
 const DB_HOST = '172.28.0.2'
-const URI = `mongodb://${DB_USER}:${DB_PSW}@${DB_HOST}:27017`;
+const URI = `mongodb://${DB_USER}:${DB_PSW}@mongo:27017`;
 mongoose.connect(URI).then(() => console.log('connect')).catch((err) => console.log('can not',err));
 app.get('/',(req, res) => res.send('<h1>--222-Hello mr.Khalil-444--</h1>'))
 app.listen(PORT,() => console.log(`App is app and running on port ${PORT}`))
